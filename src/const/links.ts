9bcf37db27f3dbec2cbcf39d2aafe55cdd638c56
stephanofer <slowdown_4@hotmail.com>
2025-11-16 13:55:31 -0500

    feat: add contact and about sections with relevant SVG icons and links

---
+++ src/const/links.ts
@@ -0,0 +1,4 @@
+export const email = "contacto@armcorporation.com";
+export const emailLink = "mailto:contacto@armcorporation.com";
+export const phoneNumber = "+51 987 654 321";
+export const phoneLink = "tel:+51987654321";
