5d58a96d227a55cc23d53ad3296a6c97d36d74a0
stephanofer <slowdown_4@hotmail.com>
2025-11-18 16:54:26 -0500

    feat: Add mobile menu component and enhance MainHead with font preloads
    - Introduced a new MobileMenu component for better navigation on mobile devices.
    - Updated MainHead component to preload custom fonts for improved performance.
    - Refactored Navbar to import MobileMenu from the correct path.
    - Updated links in the links.ts file to include project name.
    - Changed redirect paths in admin index page from "/signin" to "/ingresar".
    - Cleaned up index.astro by formatting and organizing component imports.
    - Enhanced Brands section with a carousel for brand logos and improved styling.
    - Created a backup of the original Brands section for reference.
    - Minor adjustments in Categories and FeaturedProducts sections for consistency.
    - Improved global CSS by removing commented-out code and organizing color variables.

--- src/const/links.ts
+++ src/const/links.ts
@@ -3,3 +3,4 @@ export const emailLink = "mailto:contacto@armcorporation.com";
 export const phoneNumber = "+51 987 654 321";
 export const phoneLink = "tel:+51987654321";
 export const whatsappNumeber = "+51987654321";
+export const projectName = "ARM Corporation";
