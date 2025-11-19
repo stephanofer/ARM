707a3f4137f64b0ae0f286d9de3c827330337839
stephanofer <slowdown_4@hotmail.com>
2025-11-19 17:49:01 -0500

    feat: update footer legal links and improve middleware user handling

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,18 @@
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    email: string;
+    user: import("@supabase/supabase-js").User | null;
+    isLoggedIn: boolean;
+  }
 }
 
 // /// <reference types="astro/client" />
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,27 @@ import { defineMiddleware } from "astro:middleware";
 import { createClient } from "./lib/supabase";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    const { pathname } = context.url;
+  const { pathname } = context.url;
 
-    // console.log("Middleware executing for path:", pathname);
-
-    const supabase = createClient({
-        request: context.request,
-        cookies: context.cookies,
-    });
-
-    if (pathname.startsWith("/admin")) {
-        // console.log("Checking auth for protected route");
-
-        const { data } = await supabase.auth.getUser();
-        // console.log(data);
-
-        if (!data.user) {
-            return context.redirect("/ingresar");
-        }
-    }
-
+  if (!pathname.startsWith("/admin")) {
+    console.log("pase");
     return next();
+  }
+
+  const supabase = createClient({
+    request: context.request,
+    cookies: context.cookies,
+  });
+
+  const { data } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    return context.redirect("/ingresar");
+  }
+
+  context.locals.user = data.user;
+  context.locals.email = data.user.email || "";
+  context.locals.isLoggedIn = !!data.user;
+
+  return next();
 });
